e514bd4da25123ec6c8ca36cc7ed12089e950aff
soulmars <110263471+soulmars@users.noreply.github.com>
2025-09-27 12:55:06 -0700

    Update enemy.gd

--- enemy.gd
+++ enemy.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 @export var speed: float
 @onready var target: CharacterBody2D = $"../CharacterBody2D"
-
+@onready var progress_bar: ProgressBar = $ProgressBar
+var health = 15
 
 func _process(delta: float) -> void:
+	progress_bar.value  = health
 	if target:
 		# Direction vector from follower to player
 		var direction = target.global_position - global_position
@@ -13,3 +15,11 @@ func _process(delta: float) -> void:
 		if direction.length() > 120:
 			# Move toward the player
 			position += direction.normalized() * speed * delta
+
+
+func hurt():
+	if health - 1 >= 0:
+		health -= 1
+	if health == 0:
+		queue_free()
+	
